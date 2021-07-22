4c3d08bb28868a996c9c4976f19d85c162e41aa9
shaymk1 <mkekae@gmail.com>
2021-07-22 15:47:57 +0200

    intial commit

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'store',
     'category',
     'accounts',
+    'carts',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("store.urls")),
-    # path('category/', include("category.urls")),
+    path('carts/', include("carts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
